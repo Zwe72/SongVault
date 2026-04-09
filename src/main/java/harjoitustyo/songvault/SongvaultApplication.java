cde7c85d8fbd0ef5a0042e0ffe095c10fbfe0613
bhm526@myy.haaga-helia.fi <bhm526@myy.haaga-helia.fi>
2026-04-09 12:28:33 +0300

    added base

---
+++ src/main/java/harjoitustyo/songvault/SongvaultApplication.java
@@ -0,0 +1,13 @@
+package harjoitustyo.songvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongvaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/harjoitustyo/songvault/SongvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package harjoitustyo.songvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
